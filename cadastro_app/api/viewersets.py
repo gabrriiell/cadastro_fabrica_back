b4f141a400614db9700c04289df0e32ed55bffec
Gabriel gonzaga <gabrieljppb10@gmail.com>
2024-03-09 19:55:57 -0300

    gitignore

--- cadastro_app/api/viewersets.py
+++ cadastro_app/api/viewersets.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
 from cadastro_app.api import serializers
 from cadastro_app import models
-from cadastro_app.models import cadastro  #
+from cadastro_app.models import cadastro  
 consultas_do_campos = cadastro.objects.all()
+
 #classe CadastroView é uma ViewSet pega todas as ações do crud para o modelo Cadastro 
-class cadastroview(viewsets):
+class cadastroview(viewsets.ModelViewSet):
     serializerscals = serializers.cadastroserializer
-    queryset = models.cadastro.objects.all()
+    queryset = models.cadastro.object.all()
 
